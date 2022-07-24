b0173a779e3937f3fa0a4f0cf1d4249e190dafac
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2022-07-24 16:17:29 +0300

    Added ex_10_1 README file and minor fix to ex_10_2

---
+++ ex_10_1/README.md
@@ -0,0 +1,5 @@
+# Exercise 10_1
+
+Add Bootstrap styling to addmovie.pug file that we created in this chapter.
+
+Hint! See how to style forms using the Bootstrap https://getbootstrap.com/docs/4.0/components/forms/
--- ex_10_2/index.js
+++ ex_10_2/index.js
@@ -30,6 +30,6 @@ app.post("/addcustomer", (req, res) => {
     res.redirect("/");
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${port}.`);
 });
